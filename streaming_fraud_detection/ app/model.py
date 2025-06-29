9261ecb5a0a7ce4c1f41d371dfa6e1a83543b714
JhaswinArjunSuresh <jhaswin.arjun.suresh@gmail.com>
2025-06-30 00:22:00 +0400

    Update model.py

--- streaming_fraud_detection/ app/model.py
+++ streaming_fraud_detection/ app/model.py
@@ -1,1 +1,12 @@
+import pickle
+
+# Load pre-trained fraud detection model (RandomForest)
+with open("fraud_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+def predict_transaction(features):
+    # features is a list or numpy array of model inputs
+    prediction = model.predict([features])[0]
+    prob = model.predict_proba([features])[0][1]
+    return prediction, prob
 

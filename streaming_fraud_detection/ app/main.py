e66d7c5ae15dfc6682b7a624a4ac6998dff311f8
JhaswinArjunSuresh <jhaswin.arjun.suresh@gmail.com>
2025-06-30 00:22:55 +0400

    Update main.py

--- streaming_fraud_detection/ app/main.py
+++ streaming_fraud_detection/ app/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+alerts = []
+
+@app.post("/alert")
+async def alert_endpoint(req: Request):
+    payload = await req.json()
+    alerts.append(payload)
+    print(f"🚨 Alert stored: {payload}")
+    return {"status": "alert received"}
+
+@app.get("/alerts")
+def get_alerts():
+    return {"alerts": alerts}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 

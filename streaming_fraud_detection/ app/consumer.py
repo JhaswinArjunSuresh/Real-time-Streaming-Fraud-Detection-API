9ea369bf6c4ef70e96f1d03ff70030d0cdcc9a71
JhaswinArjunSuresh <jhaswin.arjun.suresh@gmail.com>
2025-06-30 00:22:18 +0400

    Update consumer.py

--- streaming_fraud_detection/ app/consumer.py
+++ streaming_fraud_detection/ app/consumer.py
@@ -1,1 +1,24 @@
+from kafka import KafkaConsumer
+import json
+from .model import predict_transaction
+from .alert_api import send_alert
+
+def start_consumer():
+    consumer = KafkaConsumer(
+        'transactions',
+        bootstrap_servers='localhost:9092',
+        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+    )
+
+    print("🚀 Listening to Kafka topic 'transactions'...")
+    for message in consumer:
+        data = message.value
+        print(f"Received: {data}")
+
+        features = data["features"]  # Example: list of input features
+        prediction, prob = predict_transaction(features)
+
+        if prediction == 1:
+            print(f"⚠️ Fraud detected with probability {prob}")
+            send_alert(data, prob)
 

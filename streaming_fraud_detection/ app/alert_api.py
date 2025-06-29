6e76382295b34e3823b9abed96e3298a0c90a0f9
JhaswinArjunSuresh <jhaswin.arjun.suresh@gmail.com>
2025-06-30 00:22:37 +0400

    Update alert_api.py

--- streaming_fraud_detection/ app/alert_api.py
+++ streaming_fraud_detection/ app/alert_api.py
@@ -1,1 +1,13 @@
+import requests
+
+def send_alert(transaction_data, probability):
+    payload = {
+        "transaction": transaction_data,
+        "fraud_probability": probability
+    }
+    try:
+        resp = requests.post("http://localhost:8000/alert", json=payload)
+        print(f"Alert sent. Server response: {resp.text}")
+    except Exception as e:
+        print(f"Failed to send alert: {e}")
 
